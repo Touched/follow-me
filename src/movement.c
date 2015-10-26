ad9b7c26e448c2c2618138f8a0a42212c792eaf3
Touched <jtouched@gmail.com>
2015-10-26 21:40:55 +0200

    Create stairs movements

--- src/movement.c
+++ src/movement.c
@@ -15,6 +15,16 @@ u8 determine_follower_state(struct npc_state *follower, u8 state, enum direction
 	}
 
 	switch (state) {
+	case 0x08:
+	case 0x09:
+	case 0x0A:
+	case 0x0B:
+		RETURN_STATE(0x8 + direction);
+	case 0x0C:
+	case 0x0D:
+	case 0x0E:
+	case 0x0F:
+		RETURN_STATE(0xC + direction);		
 	case 0x10:
 	case 0x11:
 	case 0x12:
@@ -31,6 +41,19 @@ u8 determine_follower_state(struct npc_state *follower, u8 state, enum direction
 	case 0x3F:
 	case 0x40:
 		RETURN_STATE(0x3d + direction);
+	case 0x41:
+	case 0x42:
+	case 0x43:
+	case 0x44:
+		/* 
+		 * Running sideways on stairs does not use the slow
+		 * frames, so split this into two. 
+		 */
+		if (direction < 2) {
+			RETURN_STATE(0x41 + direction);
+		} else {
+			RETURN_STATE(0x3d + direction);
+		}
 	default:
 		return 0xFE;
 	}
