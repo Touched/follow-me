f1c950b101542d62cb842b9049e4d70dd41fe9c4
Touched <jtouched@gmail.com>
2015-10-26 16:04:04 +0200

    Apply hook for normal warp end

--- src/warps/end.s
+++ src/warps/end.s
@@ -10,5 +10,14 @@ warp_door_end_hook:
 	ldr r0, =(0x0807E200 + 1)
 	bx r0
 
+warp_normal_end_hook:
+	bl warp_set_end
+	ldr r3, =(0x08068A5C + 1)
+	bl call_via_r3
+	ldr r3, =(0x0806994C + 1)
+	bl call_via_r3
+	ldr r0, =(0x0807E310 + 1)
+	bx r0
+
 call_via_r3:
 	bx r3
