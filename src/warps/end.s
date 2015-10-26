68ecfae2ea579aed5fdc4446adac595dafe1a538
Touched <jtouched@gmail.com>
2015-10-26 16:18:45 +0200

    Apply hook for after stairs

--- src/warps/end.s
+++ src/warps/end.s
@@ -19,5 +19,15 @@ warp_normal_end_hook:
 	ldr r0, =(0x0807E310 + 1)
 	bx r0
 
+warp_stairs_end_hook:
+	bl warp_set_end	
+	ldr r3, =(0x0805FAA8 + 1)
+	bl call_via_r3
+	ldr r3, =(0x0806994C + 1)
+	bl call_via_r3
+	mov r0, r4
+	ldr r1, =(0x0807EC64 + 1)
+	bx r1
+
 call_via_r3:
 	bx r3
