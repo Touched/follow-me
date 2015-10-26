2f8ef6d41afc23f714fb487a1724b52486654590
Touched <jtouched@gmail.com>
2015-10-26 13:58:17 +0200

    Create NPC visibility helper functions

--- src/util.c
+++ src/util.c
@@ -8,3 +8,11 @@ struct npc_state *player_npc_state(void) {
 struct npc_state *follower_npc_state(void) {      
 	return &npc_states[follower_state.npcid];
 }
+
+void npc_make_invisible(struct npc_state *npc) {
+	npc->field1 |= 0x20u;
+}
+
+void npc_make_visible(struct npc_state *npc) {
+	npc->field1 &= 0xDFu;
+}
--- src/util.h
+++ src/util.h
@@ -7,5 +7,7 @@ void gf_memcpy(void *dst, void *src, u32 size);
 
 struct npc_state *player_npc_state(void);
 struct npc_state *follower_npc_state(void);
+void npc_make_invisible(struct npc_state *npc);
+void npc_make_visible(struct npc_state *npc);
 
 #endif /* PKMN_UTIL_H_ */
