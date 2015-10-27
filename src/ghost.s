a182fbe48b5e204acc9b3bfa61e70b8781946348
Touched <jtouched@gmail.com>
2015-10-27 10:46:50 +0200

    Fix collision bug (typo)
    Player could walk through moving NPCs because the wrong register was
    used to branch back.

--- src/ghost.s
+++ src/ghost.s
@@ -5,7 +5,7 @@ collision_hook:
 	push {r1-r3}
 	mov r0, r2
 	mov r1, r6
-	bl collision_exempt
+	bl collision_exempt	
 	pop {r1-r3}
 	cmp r0, #0
 	beq check_for_collision
@@ -19,8 +19,5 @@ check_for_collision:
 	ldrsh r1, [r2, r0]
 	lsl r0, r7, #0x10
 	asr r3, r0, #0x10
-	ldr r1, =(0x08063930 + 1)
-	bx r1
-
-	.align 2
-walkrun_state: .word 0x02037078
+	ldr r0, =(0x08063930 + 1)
+	bx r0
